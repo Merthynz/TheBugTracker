334888c227685098a17b749d6cf57c251a52d5df
Merthynz <rerive@gmail.com>
2022-11-21 16:04:49 -0500

    Added Model - TicketComment

---
+++ Models/TicketComment.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTracker.Models
+{
+    public class TicketComment
+    {
+        //Id
+        public int Id { get; set; }
+
+        //Comment
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        //Created
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        //TicketId
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        //UserId
+        [DisplayName("Team Memmber")]
+        public string UserId { get; set; }
+
+
+
+        // -- Navigation Properties --
+        //Ticket
+        public virtual Ticket Ticket { get; set; }
+
+        //User
+        public virtual BTUser User { get; set; }
+    }
+}
