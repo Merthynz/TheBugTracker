7694ba0daf0a846595d8a864513d8e578ab8efdc
Merthynz <rerive@gmail.com>
2022-11-27 21:39:11 -0500

    Added and registered services - IBTProjectService and BTProjectService, Added Enums - Roles

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddIdentity<BTUser, IdentityRole>(options => options.SignIn.Req
 //Custom Services
 builder.Services.AddScoped<IBTRolesService, BTRolesService>();
 builder.Services.AddScoped<IBTCompanyInfoService, BTCompanyInfoService>();
+builder.Services.AddScoped<IBTProjectService, BTProjectService>();
 
 builder.Services.AddControllersWithViews();
 
